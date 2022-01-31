4d7bfc801e8570d84fdf2841cb4ea07380240b69
Graham Billington <graahamhbillington@gmail.com>
2022-01-30 23:51:52 -0500

    cleaned code and added array merge to populate events array

--- index.js
+++ index.js
@@ -1,106 +1,30 @@
-require("dotenv").config();
-const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const util = require('./util.js');
 
 (async () => {
-
-    // open browser and go to GMHS sports schedule page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(process.env.CALENDAR_URL, {
-        waitUntil: 'networkidle2'
-    });
-
-    // get html and load it into parser (cheerio) 
-    const html = await page.content();
-
+    const html = await util.scrapeCalendar();
+    
     const $ = cheerio.load(html);
-    let eventStrings = getStringArrayOf($('.calendar-daily-event'));
-    console.log(eventStrings)
-    let locationStrings = getStringArrayOf($('.calendar-daily-location'));
-    console.log(locationStrings)
+    
+    // let eventStrings = [
+    //     '4:30 PM: JV-2 Boys Basketball Game vs North Reading',
+    //     '5:00 PM: Junior Varsity Girls Basketball Game vs North Reading',
+    //     '6:30 PM: Varsity Girls Basketball Game vs North ReadingCAL',
+    //     '7:30 PM: Varsity Boys Basketball Game vs North ReadingCAL'
+    // ] 
 
-    // $(".calendar-daily-location").each((i, elem) => {
-    //     let locationString = $(elem).text().trim();
-
-    //     if (!locationString.includes("Locaiton")) {
-    //         if (eventString.includes("\t")) {
-    //             eventString = eventString.replace(/\t/g, "")
-    //         }
-    //     }
-    // })
-
-
-
-    // loop through all events on the calendar
-    // $(".calendar-daily-event, .calendar-daily-location").each(function (i, elem) {
-
-    //     // get the game string from the event
-    //     let eventString = $(this).text().trim();
-    //     console.log(eventString)
-
-    //     // remove unwanted elements and characters from each event string
-    //     if (!eventString.includes('Event')) {
-
-    //         if (eventString.includes("\t")) {
-    //             eventString = eventString.replace(/\t/g, "")
-    //         }
-
-    //         // // manipulate string to form an Game object to be pushed into the array
-    //         // let game = {
-    //         //     time: ,
-    //         //     team: ,
-    //         //     opponent: ,
-    //         // }
-
-
-    //         // populate array of games
-    //         eventStrings.push(eventString)
-
-    //     }
-    // })
-    // console.log(gameStrings)
-    // get array of today's games from calendar 
-    // let dailySportsCalendar = $(".calendar-daily-event").html()
-    // console.log(dailySportsCalendar)
-    // .replace('/(\\t|\s)+/g', 'nothign');
-    // dailySportsCalendar = dailySportsCalendar.map(game => {
-    //     // console.log(game.trim())
-    //     game.trim();
-    //     return game.replace(/\\t/g, '');
-
-    // })
-
-
-
-
-
-    await browser.close();
-
-    // takes in array of cheerio objects and returns array of strings
-    function getStringArrayOf(array) {
-
-        let stringArray = [];
-        array.each((i, elem) => {
-
-            let string = $(elem).text().trim();
-
-            if (!string.includes($(array[0]).text().trim())) {
-                if (string.includes("\t")) {
-                    string = string.replace(/\t/g, "")
-                }
-
-                stringArray.push(string)
-
-            }
-        })
-
-        return stringArray;
-
-    }
-
-
+    let eventStrings = util.getStringArrayOf($, $('.calendar-daily-event'));
+    
+    // let locationStrings = [
+    //     'North Reading High School',
+    //     'Georgetown High School',
+    //     'Georgetown High School',
+    //     'North Reading High School'
+    // ]
+    let locationStrings = util.getStringArrayOf($, $('.calendar-daily-location'));
+    
+    // merge events and locations into an array of event objects
+    let events = util.mergeEvents(eventStrings, locationStrings)
+    
+    console.log(events)
 })();
-
