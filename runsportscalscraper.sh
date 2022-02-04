3d73ec5801c4368abfb39a9c30d8c4b30aa81a51
Graham Billington <graahamhbillington@gmail.com>
2022-02-03 21:06:40 -0500

    changed file to test autodeployment

--- runsportscalscraper.sh
+++ runsportscalscraper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 # this file is copied over to /usr/bin/ when deployed to server
 # the actual script used in cronjob stored in /usr/bin/
 /usr/local/bin/node /home/graham/sports-calendar-scraper/index.js >> /home/graham/cron.log
