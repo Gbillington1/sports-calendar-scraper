7a287189b8eb90ac5d03b34ec9d516e8f85359d6
Graham Billington <graahamhbillington@gmail.com>
2022-02-03 21:08:26 -0500

    added autodeployment to RPI

--- runsportscalscraper.sh
+++ runsportscalscraper.sh
@@ -2,4 +2,3 @@
 # this file is copied over to /usr/bin/ when deployed to server
 # the actual script used in cronjob stored in /usr/bin/
 /usr/local/bin/node /home/graham/sports-calendar-scraper/index.js >> /home/graham/cron.log
-#big boi test comment
