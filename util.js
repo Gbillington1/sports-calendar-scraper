d0855b52eed30d0688f62deed6a2349081082558
Rida F'kih <hello@rida.dev>
2022-02-04 12:49:31 -0700

    cleanup merge events function

--- util.js
+++ util.js
@@ -55,56 +55,24 @@ function getStringArrayOf($, array) {
  * @returns {{ time: string; team: string; date: string; opponent: string; location: string; home: string }[]} An array of event objects.
  */
 function mergeEvents($, eventStrings, locationStrings) {
-
-    // error handling
     if (eventStrings.length != locationStrings.length) {
         return new Error("Event Strings and Location Strings are not the same length");
     }
-    
-    let events = [];
 
     // Get date from calendar element in html (date relative to page, not machine)
-    let dateElem = removeTabs($(".daily-calendar").text().trim());
-    let date = dateElem.substring(0, dateElem.search(/\n/));
+    const dateElem = removeTabs($(".daily-calendar").text().trim());
+    const date = dateElem.substring(0, dateElem.search(/\n/));
 
-    // loop through each event string and location string
-    // populate array of event objects
-    eventStrings.forEach((eventString, i) => {
+		const events = eventStrings.map((eventString, index) => {
+			const time = eventString.substring(0, eventString.search(" ") + 3);
+			const team = eventString.substring(eventString.search(" ") + 5, eventString.search("vs") - 6);
+			const opponent = eventString.substring(eventString.search("vs") + 3, eventString.length);
+			const location = locationStrings[index];
+			const home = location.includes("Georgetown");
+			return { time, team, date, opponent, location, home };
+		})
 
-        let event = {
-            time: "",
-            date: "",
-            team: "",
-            opponent: "",
-            location: "",
-            home: false
-        }
-
-        // time from string "4:30 PM"
-        event.time = eventString.substring(0, eventString.search(" ") + 3);
-
-        // current day of the week
-        event.date = date
-
-        // team from left side of "vs" in the left side of the string
-        event.team = eventString.substring(eventString.search(" ") + 5, eventString.search("vs") - 6);
-
-        // opponent from right side of "vs" in event string
-        event.opponent = eventString.substring(eventString.search("vs") + 3, eventString.length);
-
-
-        event.location = locationStrings[i];
-
-        if (event.location.includes("Georgetown")) {
-            event.home = true;
-        }
-
-        events.push(event)
-
-    })
-
-    return events;
-
+		return events;
 }
 
 function removeTabs(string) {
