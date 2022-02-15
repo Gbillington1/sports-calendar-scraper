99fde5e063edce9a8d072e5f6c071fb6cd0db761
Graham Billington <graahamhbillington@gmail.com>
2022-02-15 18:20:11 -0500

    edited dotenv config

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path: __dirname + "/.env"});
+require("dotenv").config({path: __dirname + "/.env", silent: true});
 
 const cheerio = require('cheerio');
 const util = require('./util.js');
