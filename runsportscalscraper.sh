325c20d1e5a5035374083f840356c5343129fcb7
Graham Billington <graahamhbillington@gmail.com>
2022-02-16 21:54:12 -0500

    added production node env to beginning of node script instanciation

--- runsportscalscraper.sh
+++ runsportscalscraper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # this file is copied over to /usr/bin/ when deployed to server
 # the actual script used in cronjob stored in /usr/bin/
-/usr/local/bin/node /home/graham/sports-calendar-scraper/index.js >> /home/graham/sports-calendar-scraper/cron.log
+NODE_ENV=production /usr/local/bin/node /home/graham/sports-calendar-scraper/index.js >> /home/graham/sports-calendar-scraper/cron.log
