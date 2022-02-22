142480285e8e4baae87198ff5ae2ebe2022c8234
Graham Billington <graahamhbillington@gmail.com>
2022-02-21 19:48:08 -0500

    commented out working functionality to experiement with eventParsing.sj

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const cheerio = require('cheerio');
+const eventParsing = require('./eventParsing.js');
 const util = require('./util.js');
 const mail = require('./mail.js');
 
@@ -13,33 +14,37 @@ const mail = require('./mail.js');
 
     const $ = cheerio.load(html);
 
-    const eventStrings = util.getStringArrayOf($, $('.calendar-daily-event'));
+    const events = eventParsing.getEvents($);
+
+    // const eventStrings = util.getStringArrayOf($, $('.calendar-daily-event'));
 
     // light error handling
     // TODO: email me this string
-    if (eventStrings.length == 0) {
-        mail.sendEmail(`${process.env.HOME_TOWN} Sports Calendar`, "No events today");
-        return
-    }
+    // if (eventStrings.length == 0) {
+    //     mail.sendEmail(`${process.env.HOME_TOWN} Sports Calendar`, "No events today");
+    //     return
+    // }
 
     // todo: find a way to handle postponed games and canceled games
     // todo: find a way to handle special events (Wrestling, swimming, golf, etc.) 
-    if (eventStrings[eventStrings.length - 1].includes("Postponed")) {
-        console.log("This event has been postponed.")
-    }
+    // if (eventStrings[eventStrings.length - 1].includes("Postponed")) {
+    //     console.log("This event has been postponed.")
+    // }
     
-    const locationStrings = util.getStringArrayOf($, $('.calendar-daily-location'));
+    // const locationStrings = util.getStringArrayOf($, $('.calendar-daily-location'));
+
+    // console.log(eventStrings)
     
     // merge events and locations into an array of event objects
-    const events = util.mergeEvents($, eventStrings, locationStrings)
+    // const events = util.mergeEvents($, eventStrings, locationStrings)
 
     // generate announcements from events
-    const announcements = mail.generateAnnouncementsFromEvents(events);
+    // const announcements = mail.generateAnnouncementsFromEvents(events);
 
     // get email body from announcements
-    const announcementString = mail.getAnnouncementString(announcements);
+    // const announcementString = mail.getAnnouncementString(announcements);
 
     // send email to my address with the events in announement readable format
-    mail.sendEmail("Today's Sports News", announcementString);
+    // mail.sendEmail("Today's Sports News", announcementString);
     
 })();
